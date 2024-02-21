def75351051920269e5c217649dcd49a9233769e
MarouaneSaoud <marouanesaoud84@gmail.com>
2024-02-21 16:28:17 +0100

    first commit

---
+++ src/main/java/com/veri_delice/gestion_cmd_vd_backend/GestionCmdVdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.veri_delice.gestion_cmd_vd_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCmdVdBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionCmdVdBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/veri_delice/gestion_cmd_vd_backend/GestionCmdVdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veri_delice.gestion_cmd_vd_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCmdVdBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
